823a17962a09ac17cc9414aef86d1d6b448cd0f6
alperenkaracete <m.alperenk@gmail.com>
2026-05-09 02:00:29 +0300

    Add hw2

---
+++ java-odevler/src/main/java/n11bootcamp/odev2/odev2.java
@@ -0,0 +1,32 @@
+package n11bootcamp.odev1;
+
+import java.util.Scanner;
+
+public class odev2 {
+    public static void main(String[] args) {
+
+        double tutar, kdvOrani, kdvTutari, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz: ");
+        tutar = input.nextDouble();
+
+
+        if (tutar > 0 && tutar <= 1000) {
+            kdvOrani = 0.18;
+        } else {
+            kdvOrani = 0.08;
+        }
+
+        kdvTutari = tutar * kdvOrani;
+        kdvliTutar = tutar + kdvTutari;
+
+
+        System.out.println("KDV'siz Tutar: " + tutar);
+        System.out.println("KDV Oranı: %" + (int)(kdvOrani * 100));
+        System.out.println("KDV Tutarı: " + kdvTutari);
+        System.out.println("KDV'li Tutar: " + kdvliTutar);
+
+        input.close();
+    }
+}
